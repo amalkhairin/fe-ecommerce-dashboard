33da60607f6b2012982c0f561b7007e730533b32
amalkhairinn <amalkhairinn@gmail.com>
2024-08-27 11:59:23 +0700

    update counterpage

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment } from '../redux/counter/counterSlice'
+import { decrement, increment, incrementByAmount } from '../redux/counter/counterSlice'
+import { Button, Card, CardBody, CardHeader, Slider } from '@nextui-org/react';
+import { MinusIcon, PlusIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 
 export function CounterPage() {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
+    const [incementAmount, setIncrementAmount] = useState(0)
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
 
-  return (
-    <div>
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
-    </div>
-  )
+    return (
+        <div className='flex items-center justify-center h-screen bg-background-gray'>
+            <Card className='w-96 shadow-lg' radius='md'>
+                <CardHeader className='flex justify-center'>
+                    <h2 className='text-2xl font-bold text-primary'>Counter App</h2>
+                </CardHeader>
+                <CardBody className='flex justify-center flex-col items-center space-y-6'>
+                    <h3 className='text-4xl font-bold text-primary-dark'>{count}</h3>
+                    <div className='flex space-x-4'>
+                        <Button color="danger" variant="shadow" onClick={() => dispatch(decrement())} startContent={<MinusIcon className='h-6 w-6' />}
+                        >Decrement</Button>
+                        <Button color="success" variant="shadow" onClick={() => dispatch(increment())} startContent={<PlusIcon className='h-6 w-6' />}>Increment</Button>
+                    </div>
+                    <div className='w-full space-y-2'>
+                        <Slider size='sm' step={1} color='primary' label='amount' showSteps={true} maxValue={10} className='max-w-md ' onChange={(value) => setIncrementAmount(value)} />
+                        <div className='flex justify-between items-center'>
+                            <span className='text-sm text-primary-dark'>amount: {incementAmount}</span>
+                            <Button color='primary' variant='shadow' onPress={() => {
+                                dispatch(incrementByAmount(Number(incementAmount)))
+                            }} size='sm' >Add Amount</Button>
+                        </div>
+                    </div>
+                </CardBody>
+            </Card>
+        </div>
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-import { COLORS } from './src/constants/colors.constant';
+import { nextui } from "@nextui-org/react";
+import { COLORS } from "./src/constants/colors.constant";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,18 +15,18 @@ export default {
           DEFAULT: COLORS.PRIMARY,
           dark: COLORS.PRIMARY_DARK,
           light: COLORS.PRIMARY_LIGHT,
-          darker: COLORS.PRIMARY_DARKER
+          darker: COLORS.PRIMARY_DARKER,
         },
         text: {
           white: COLORS.TEXT_WHITE,
-          gray: COLORS.TEXT_GRAY
+          gray: COLORS.TEXT_GRAY,
         },
         background: {
-          gray: COLORS.BACKGROUND_GRAY
-        }
-      }
+          gray: COLORS.BACKGROUND_GRAY,
+        },
+      },
     },
   },
-  plugins: [],
-}
-
+  darkMode: "class",
+  plugins: [nextui()],
+};
