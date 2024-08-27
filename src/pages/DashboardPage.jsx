d38b386e5e8a433f17a54be320172d64a0fa7757
amalkhairinn <amalkhairinn@gmail.com>
2024-08-27 10:18:30 +0700

    add side nav

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -66,13 +66,19 @@ function DashboardPage() {
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="flex-1 flex flex-col overflow-y-auto">
-          <div className="flex-1 px-2 py-4 space-y-1">
-            {navigation.map((item) => {
-              return <NavLink key={item.name} to={item.href} className={({ isActive }) => (isActive ? "bg-primary-dark text-text-white" : "text-text-gray hover:bg-primary-light hover:text-text-white" + "group flex items-center px-2 py-2 text-sm font-medium rounded-md")}></NavLink>
-            })}
+          <nav className=" flex-1 flex flex-col overflow-y-auto">
+            <div className="flex-1 px-2 py-4 space-y-1">
+              {navigation.map((item) => {
+                const IconComponent = item.icon;
+                return <NavLink key={item.name} to={item.href} className={({ isActive }) => (isActive ? "bg-primary-dark text-text-white" : "text-text-gray hover:bg-primary-light hover:text-text-white" + "group flex items-center px-2 py-2 text-sm font-medium rounded-md")}>
+                  <IconComponent className="mr-3 h-6 w-6 flex-shrink-0" />
+                  <span className="hidden md:inline">{item.name}</span>
+                </NavLink>
+              })}
+            </div>
+          </nav>
+          <div className="flex-shrink-0 flex flex-col border-t border-primary-light p-4">
+            <button onClick={logoutHandler} className="w-full group flex items-center px-2 py-2 text-sm font-medium rounded-md text-text-gray hover:bg-primary-light hover:text-text-white">Logout</button>
           </div>
         </div>
       </div>
