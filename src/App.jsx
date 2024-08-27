d6575156aa12e2acb624b19206b144af50989e51
amalkhairinn <amalkhairinn@gmail.com>
2024-08-27 11:07:20 +0700

    add redux

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./components/ProtectedRoute";
-import SimpleDashboardPage from "./pages/SimpleDashboardPage";
 import DashboardPage from "./pages/DashboardPage";
+import LoginPage from "./pages/LoginPage";
+import { CounterPage } from "./pages/CounterPage";
 
 const HomePage = () => <h1>Home Page</h1>;
 const UsersPage = () => <h1>Users Page</h1>;
 const ProductsPage = () => <h1>Products Page</h1>;
 const TransactionsPage = () => <h1>Transactions Page</h1>;
-const CounterPage = () => <h1>Counter Page</h1>;
 
 const router = createBrowserRouter([
   {
